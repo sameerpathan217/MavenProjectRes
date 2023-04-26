df200a7df2ee17ee40ff0c60a7f993f0a5b1c0db
sameerpathan217 <sameerpathan217@gmail.com>
2023-04-26 17:26:00 +0530

    commit 2

--- MavenPractice/src/test/java/DemoTest/Parameter.java
+++ MavenPractice/src/test/java/DemoTest/Parameter.java
@@ -10,14 +10,9 @@ public class Parameter {
 	@Test
 	public void display() {
 	
-		String BROWSER = System.getProperty("browser");
-		String URL = System.getProperty("url");
-		System.out.print(BROWSER);
-		System.out.println(URL);
-		
 		System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.get(URL);
+		
 		
 		
 	}
